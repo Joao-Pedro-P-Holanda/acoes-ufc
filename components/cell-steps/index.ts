7448ebc001a93467ca545467690c770cdad231d9
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2025-11-09 22:53:09 -0300

    feat: add index file to export cell step components

---
+++ components/cell-steps/index.ts
@@ -0,0 +1,5 @@
+export { BasicInfoStep } from './basic-info-step';
+export { DetailsStep } from './details-step';
+export { GroupTypeStep } from './group-type-step';
+export { OrganizersStep } from './organizers-step';
+
